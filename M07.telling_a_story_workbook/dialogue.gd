ec75f19a93f456ab3b887ac38399160b37b484fb
telaw1 <telaw1@s.sfusd.edu>
2025-11-07 11:03:51 -0800

    completed M7L5

---
+++ M07.telling_a_story_workbook/dialogue.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+
+
+var dialogue_items: Array[String] = [
+	"Hello!",
+	"What's happening right now?",
+	"SHIBAL.",
+	"HEHE!",
+	"Okay! Bye-Bye!",
+]
+
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+
+func advance() -> void:
+	current_item_index += 1
+	
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
